e15fddaa206a62c5b22d07a3e8f328836b4fb28c
Rofus06 <Filipsmail@yahoo.com>
2024-03-25 09:15:30 +0100

    Tried to make a random number but did not work

--- Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
+++ Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb3e14345ac3094c14ec7a8c2cd5632146a2e11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a207b13721f7d41ec6c53048d489f569f9159b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
